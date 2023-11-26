8064f8acaab291120ed2c985106325e0e2c9c67b
Aptivi CEO <eoflaoevicecity@gmail.com>
2023-11-26 09:40:31 +0300

    upd - Updated workflows for .NET 8.0

--- tools/pack.sh
+++ tools/pack.sh
@@ -17,7 +17,7 @@ fi
 # Pack binary
 echo Packing binary...
 cd "../Dictify/bin/$releaseconf/netstandard2.0/" && "$zippath" -r /tmp/$version-bin.zip . && cd -
-cd "../Dictify.Demo/bin/$releaseconf/net6.0/" && "$zippath" -r /tmp/$version-demo.zip . && cd -
+cd "../Dictify.Demo/bin/$releaseconf/net8.0/" && "$zippath" -r /tmp/$version-demo.zip . && cd -
 if [ ! $? == 0 ]; then
 	echo Packing using zip failed.
 	exit 1
